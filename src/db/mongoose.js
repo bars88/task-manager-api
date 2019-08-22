95ddc67abd96a1f136cc34c3b285ea44e6a4d374
Boris <boris.feoktistov@yojji.io>
2019-08-22 17:38:26 +0300

    changes url constant

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://${process.env.MONGODB_CONNECT_PATH}`, {
+mongoose.connect(process.env.MONGODB_CONNECT_PATH, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
