ba88a5eb009aae1a071cfbd16fdb8e13f6e3446d
Boris <boris.feoktistov@yojji.io>
2019-08-27 11:26:24 +0300

    install cors

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ const express = require('express');
 require('./db/mongoose');
 const taskRouter = require('./routers/task');
 const userRouter = require('./routers/user');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
